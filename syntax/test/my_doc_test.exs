f90731400a5c662290ac116f69c6ddb11a914ec5
Peiling Ding <peding@arcblock.io>
2018-07-09 15:20:42 -0700

    Add test setup.

--- syntax/test/my_doc_test.exs
+++ syntax/test/my_doc_test.exs
@@ -1,4 +1,18 @@
 defmodule My.Doc.Test do
   use ExUnit.Case
   doctest My.Doc
+
+  setup do
+    {:ok, data} = prepare_data()
+    %{prepared_data: data}
+  end
+
+  def prepare_data() do
+    {:ok, []}
+  end
+
+  test("Test 1", %{prepared_data: data}) do
+    # now you can use data as you wish.
+    assert is_list(data) == true
+  end
 end
