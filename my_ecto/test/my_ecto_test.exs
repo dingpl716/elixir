e471efe0b3c7abdfc8c5ae6bbb8d8c4e09af6651
Peiling Ding <peding@arcblock.io>
2019-01-09 13:51:46 -0800

    Move my_ecto, my_flow out of this repo.

--- my_ecto/test/my_ecto_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MyEctoTest do
-  use ExUnit.Case
-  doctest MyEcto
-
-  test "greets the world" do
-    assert MyEcto.hello() == :world
-  end
-end
--- my_flow/test/my_flow_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MyFlowTest do
-  use ExUnit.Case
-  doctest MyFlow
-
-  test "greets the world" do
-    assert MyFlow.hello() == :world
-  end
-end
